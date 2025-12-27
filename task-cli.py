f19799f007a5128eb82025573274f8a8e5bf0827
Nguyen Phat <nguyenphat13112006@gmail.com>
2025-12-27 10:51:29 +0700

    list update and delete

--- task-cli.py
+++ task-cli.py
@@ -17,9 +17,7 @@ filename = "task_list.json"
 data = load_data(filename)
 if len(sys.argv) > 1:
     if sys.argv[1] == "add":
-        if len(sys.argv) < 3:
-            print("Error: Please provide a task description.")
-        else:
+        if len(sys.argv) == 3:
             new_task = {
                 "id": data["next_id"],
                 "description": sys.argv[2],
@@ -31,5 +29,32 @@ if len(sys.argv) > 1:
             data["next_id"] += 1
             save_data(filename, data)
             print(f"Task added successfully (ID: {new_task["id"]})")
+    elif sys.argv[1] == "list":
+        if len(sys.argv) == 2:
+            print(f"{"ID":5} {"Description":15} {"Status":10} {"Created At":25} {"Updated At":25}")
+            for task in data["tasks"]:
+                print(f"{str(task["id"]):5} {task["description"]:15} {task["status"]:10} {task["createdAt"]:25} {task["updatedAt"]:25}")
+        elif len(sys.argv) == 3:
+            print(f"{"ID":5} {"Description":15} {"Status":10} {"Created At":25} {"Updated At":25}")
+            for task in data["tasks"]:
+                if task["status"] == sys.argv[2]:
+                    print(f"{str(task["id"]):5} {task["description"]:15} {task["status"]:10} {task["createdAt"]:25} {task["updatedAt"]:25}")
+    elif sys.argv[1] == "update":
+        if len(sys.argv) == 4:
+            for task in data["tasks"]:
+                if str(task["id"]) == sys.argv[2]:
+                    task["description"] = sys.argv[3]
+                    save_data(filename, data)
+                    print(f"Task updated successfully (ID: {task["id"]})")
+                    break
+    elif sys.argv[1] == "delete":
+        if len(sys.argv) == 3:
+            for task in data["tasks"]:
+                if str(task["id"]) == sys.argv[2]:
+                    data["tasks"].remove(task)
+                    save_data(filename, data)
+                    print(f"Task deleted successfully (ID: {task["id"]})")
+                    break
 else:
     print("Usage: python script.py add <description>")
+
